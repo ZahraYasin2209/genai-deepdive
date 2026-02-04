3611b1e56a152603de77fee787931f69a34811fb
Zahra Yasin <109916495+ZahraYasin2209@users.noreply.github.com>
2026-02-04 15:09:36 +0500

    Remove LANGCHAIN API key configuration
    Removed environment variable setup for LANGCHAIN API.

--- day-09-ReAct-Agent/app.py
+++ day-09-ReAct-Agent/app.py
@@ -1,5 +1,4 @@
 import uuid
-import os
 
 from dotenv import load_dotenv
 
@@ -11,13 +10,6 @@ import constants
 import session_manager
 import streamlit as st
 
-
-if "LANGCHAIN_API_KEY" in st.secrets:
-    os.environ["LANGCHAIN_TRACING_V2"] = "true"
-    os.environ["LANGCHAIN_ENDPOINT"] = "https://api.smith.langchain.com"
-    os.environ["LANGCHAIN_API_KEY"] = st.secrets["LANGCHAIN_API_KEY"]
-    os.environ["LANGCHAIN_PROJECT"] = st.secrets.get("LANGCHAIN_PROJECT", "Nexa-ChatBot-Testing")
-    
 
 def main():
     st.set_page_config(
